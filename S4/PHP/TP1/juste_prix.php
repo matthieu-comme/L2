2ca677deb8bd43514e2ff7c4390096fd615b20d4
Rouduriguez <125394906+Rouduriguez@users.noreply.github.com>
2025-01-23 09:34:08 +0100

    Update juste_prix.php

--- S4/PHP/TP1/juste_prix.php
+++ S4/PHP/TP1/juste_prix.php
@@ -2,10 +2,16 @@
 <body>
 <h2 align=center>Jeu : plus grand, plus petit</h2>
 <?php // FIXER LE PROBLEME DE RANDOM !!!!!
-if (!isset($_POST["a_trouver"])) { //
-	$a_trouver = mt_rand(0,100);
-	$_POST['a_trouver'] = $a_trouver;
-}  
+if (!isset($_POST["a_trouver"])) 
+	$a_trouver = mt_rand(0,100);	 
+else 
+	$a_trouver = $_POST['a_trouver'] ;
+if (isset($_POST['propose'])) {
+	$n = $_POST['propose'];
+	if($n < $_POST['a_trouver']) echo "c'est +"; 
+	else if ($n > $_POST['a_trouver']) echo "c'est -";
+	else echo "VICTOIRE";
+}
 ?>
 <form method="post" action="juste_prix.php">
 	Proposez un prix entre 0 et 100 :
@@ -13,13 +19,5 @@ if (!isset($_POST["a_trouver"])) { //
 	<input type="hidden" name="a_trouver" value="<?php echo $a_trouver;?>">
 	<input type="submit" name="submit" value="Essayer">
 </form>
-<?php
-if (isset($_POST['propose'])) {
-	$n = $a_trouver;
-	if($n < $_POST['a_trouver']) echo "c'est +"; 
-	else if ($n > $_POST['a_trouver']) echo "c'est -";
-	else echo "VICTOIRE";
-}
-?>
 </body>
 </html>
